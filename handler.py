2e539243f09f77e19e3d702649e95ae680e5acd6
celeron504504 <yuencherhlew@yahoo.co.uk>
2025-12-16 16:39:47 +0800

    Update handler.py

--- handler.py
+++ handler.py
@@ -73,14 +73,13 @@ def handler(event):
             top_k=50,
             top_p=0.8,
             do_sample=False,
-            eos_token_id=tokenizer.eos_token_id,
             pad_token_id=tokenizer.eos_token_id
         )
 
-    response_ids = output[0][inputs["input_ids"].shape[-1]:]
-    response = tokenizer.decode(response_ids, skip_special_tokens=True)
+    #response_ids = output[0][inputs["input_ids"].shape[-1]:]
+    #response = tokenizer.decode(response_ids, skip_special_tokens=True)
     
-    #response = tokenizer.decode(output[0], skip_special_tokens=True)
+    response = tokenizer.decode(output[0], skip_special_tokens=True)
     return {"message": response.split("Assistant:")[-1].strip()}
 
 
